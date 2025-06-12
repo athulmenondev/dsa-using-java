02e75941ad2283a4296ab11621266854fec48034
Athul S Menon <athulmenondev@gmail.com>
2025-06-12 17:14:40 +0530

    updated Quick sort

--- backtracking/Nqueens.java
+++ backtracking/Nqueens.java
@@ -6,35 +6,35 @@ public class Nqueens {
 
     public static boolean isSafe(int row,int col,char [][] chessboard){
         for(int i=0;i<chessboard.length;i++){
-            if (chessboard[i][col]=='Q') {
+            if (chessboard[i][col]=='Q') {//checking the row
                 return false;
             }
         }
         for(int j=0;j<chessboard.length;j++){
-            if (chessboard[row][j]=='Q') {
+            if (chessboard[row][j]=='Q') {//checking the colomn
                 return false;
             }
         }
         int i=row;
-        for(int j=col;i>=0 && j>=0;i--,j--){//upperlt
+        for(int j=col;i>=0 && j>=0;i--,j--){//upperleftt
             if (chessboard[i][j]=='Q') {
                 return false;
             }
         }
         i=row;
-        for(int j=col;i>=0&& j<chessboard.length;i--,j++){//upperrt
+        for(int j=col;i>=0&& j<chessboard.length;i--,j++){//upperrightt
             if (chessboard[i][j]=='Q') {
                 return false;
             }
         }
         i=row;
-        for(int j=col;i<chessboard.length&&j>=0;i++,j--){//lowerlt
+        for(int j=col;i<chessboard.length&&j>=0;i++,j--){//lowerleftt
             if (chessboard[i][j]=='Q') {
                 return false;
             }
         }
         i=row;
-        for(int j=col;i<chessboard.length && j<chessboard.length;i++,j++){//lowerrt
+        for(int j=col;i<chessboard.length && j<chessboard.length;i++,j++){//lowerrightt
             if (chessboard[i][j]=='Q') {
                 return false;
             }
@@ -46,7 +46,7 @@ public class Nqueens {
          List<String> newboard=new ArrayList<>();
          for(int i=0;i<chessboard.length;i++){
             row="";
-            for(int j=0;j<chessboard[0].length;j++){
+            for(int j=0;j<chessboard[0].length;j++){//saving the each row of board 
                 if( chessboard[i][j]=='Q'){
                     row+='Q';
                 }
@@ -54,20 +54,20 @@ public class Nqueens {
                     row+='.';
                 }
             }
-            newboard.add(row);
+            newboard.add(row);//adding the row to board
          }
-         allboard.add(newboard);
+         allboard.add(newboard);//ading the board to the boardlist
 
     }
     public static void helper(char [][] chessboard,List<List<String>> allboard,int col){
-        if(col==chessboard.length){
-            saveBoard(chessboard,allboard);
+        if(col==chessboard.length){//limiting condition
+            saveBoard(chessboard,allboard);//saving the correct board
             return;
         }
         for(int row=0;row<chessboard.length;row++){
-            if (isSafe(row,col,chessboard)) {
+            if (isSafe(row,col,chessboard)) {//validity checking
                 chessboard[row][col]='Q';
-                helper(chessboard, allboard, col+1);
+                helper(chessboard, allboard, col+1);//recursive call
                 chessboard[row][col]='.';
             }
         }
@@ -78,7 +78,7 @@ public class Nqueens {
         int n=sc.nextInt();
         char [][] chessboard = new char[n][n];
         List<List<String>> allboards=new ArrayList<>();
-        helper(chessboard,allboards,0);
+        helper(chessboard,allboards,0);//initial call
         System.out.println(allboards);
         sc.close();
     }
--- sorting/QuickSort.java
+++ sorting/QuickSort.java
@@ -4,26 +4,32 @@ import java.util.Scanner;
 
 public class QuickSort {
     public static void quickSort(int [] arr,int first ,int last){
-        if(last-first+1==1){
+        if(first>=last){
             return;
         }
-        int pivot = first;
+        int pivot = arr[first];//setting the pivot element
         int l=first+1;
         int r=last;
         int temp;
-        while(l<r){
-            while(arr[l]<=arr[pivot]){
+        while(l<=r){
+            while(l<=last && arr[l]<=pivot){//move left pointer
                 l++;
-            }
-            while(arr[r]>arr[pivot]){
+            }//8
+            while(r>first && arr[r]>pivot){//move right pointer
                 r--;
-            }
+            }//3
             if (l<r) {
+                System.out.println("internal swap between "+ arr[l]+" and "+arr[r]);//in between swap
                 temp=arr[l];
                 arr[l]=arr[r];
                 arr[r]=temp;
             }
         }
+        System.out.println("external swap between "+ pivot+" and "+arr[r]);//swaping between pivot
+        arr[first]=arr[r];
+        arr[r]=pivot;
+        quickSort(arr, first, r-1);//next call for 1st half
+        quickSort(arr, r+1, last);//next call for 2nd half
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
@@ -34,7 +40,12 @@ public class QuickSort {
         for (int i = 0; i < n; i++) {
             arr[i]=sc.nextInt();
         }
-        quickSort(arr,0,n-1);
+        quickSort(arr,0,n-1);//inital call
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+        sc.close();
     }
+
     
 }
