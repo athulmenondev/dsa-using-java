422bc76a65a6ab322da9afe6b501a943e63f4ade
Athul S Menon <athulmenondev@gmail.com>
2025-06-22 17:01:10 +0530

    updated BST

--- BST/BST.java
+++ BST/BST.java
@@ -1,5 +1,7 @@
 package BST;
 
+import java.util.ArrayList;
+
 public class BST {
     public static class Node {
         int data;
@@ -85,6 +87,39 @@ public class BST {
             }
             return  root;
         }
+        public static void printRange(Node root,int x, int y){
+            if(root==null){
+                return;
+            }
+            if(root.data>=x && root.data<=y){
+                printRange(root.left,x,y);
+                System.out.print(root.data+"   ");
+                printRange(root.right,x,y);
+            } else if (y<=root.data) {
+                printRange(root.left,x,y);
+            }else{
+                printRange(root.right,x,y);
+            }
+        }
+        public static void printPath(ArrayList<Integer> paths){
+            for (int i = 0; i < paths.size(); i++) {
+                System.out.print(paths.get(i)+"--->");
+            }
+            System.out.println();
+        }
+        public static void findPath(Node root, ArrayList<Integer> paths){
+            if(root==null){
+                return;
+            }
+            paths.add(root.data);
+            if (root.left ==null && root.right==null){
+                printPath(paths);
+            }else {
+                findPath(root.left,paths);
+                findPath(root.right,paths);
+            }
+            paths.remove(paths.size()-1);
+        }
     }
     public static void main(String[] args) {
         int Values[]={5,1,7,6,8,3,9};
@@ -94,8 +129,10 @@ public class BST {
         }
         Node.inOrder(root);
         //System.out.println(Node.searchBst(root,2));
-        root=Node.deleteNode(root,8);
+        //root=Node.deleteNode(root,8);
         System.out.print("\n");
-        Node.inOrder(root);
+        //Node.inOrder(root);
+        //Node.printRange(root,5,8);
+        Node.findPath(root,new ArrayList<Integer>());
     }
 }
